654a08f17141558230ebed602f2b4c916d9953d6
AnotherMelvin <41257543+AnotherMelvin@users.noreply.github.com>
2023-10-14 02:15:33 +0700

    Add CreateEditActivity activity

--- app/src/main/java/id/ac/umn/melvintungadi/map_project_group2/CreateEditActivity.kt
+++ app/src/main/java/id/ac/umn/melvintungadi/map_project_group2/CreateEditActivity.kt
@@ -1,10 +1,9 @@
 package id.ac.umn.melvintungadi.map_project_group2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CreateEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_edit)
