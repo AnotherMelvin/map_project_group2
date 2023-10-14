3a4032119ab892357f0d633035c37e63b02c1840
AnotherMelvin <41257543+AnotherMelvin@users.noreply.github.com>
2023-10-14 07:31:07 +0700

    Navigation demo

--- app/src/main/java/id/ac/umn/melvintungadi/map_project_group2/CreateEditActivity.kt
+++ app/src/main/java/id/ac/umn/melvintungadi/map_project_group2/CreateEditActivity.kt
@@ -2,10 +2,17 @@ package id.ac.umn.melvintungadi.map_project_group2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class CreateEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_edit)
+
+        if (intent != null) {
+            val name = intent.getStringExtra("NAME")
+            val nameText = findViewById<TextView>(R.id.item_name)
+            nameText.text = name
+        }
     }
 }
--- app/src/main/java/id/ac/umn/melvintungadi/map_project_group2/MainActivity.kt
+++ app/src/main/java/id/ac/umn/melvintungadi/map_project_group2/MainActivity.kt
@@ -3,10 +3,30 @@ package id.ac.umn.melvintungadi.map_project_group2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+    private val addButton: Button
+        get() = findViewById(R.id.add_item)
+
+    private val cardItem: CardView
+        get() = findViewById(R.id.card_item1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_edit)
+        setContentView(R.layout.activity_main)
+
+        addButton.setOnClickListener {
+            val addIntent = Intent(this, CreateEditActivity::class.java)
+            addIntent.putExtra("NAME", "")
+            startActivity(addIntent)
+        }
+
+        cardItem.setOnClickListener {
+            val editIntent = Intent(this, CreateEditActivity::class.java)
+            editIntent.putExtra("NAME", "Placeholder")
+            startActivity(editIntent)
+        }
     }
 }
